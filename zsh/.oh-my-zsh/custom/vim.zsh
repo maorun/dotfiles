c8a2e11430bfd2d9279ed34a7ef4d47c43b9b2da
Marco Driemel <marco.driemel@axelspringer.com>
2022-11-16 13:12:25 +0100

    add vi to zsh

---
+++ zsh/.oh-my-zsh/custom/vim.zsh
@@ -0,0 +1,40 @@
+# see https://thevaluable.dev/zsh-install-configure-mouseless/#editing-command-lines-in-vim
+
+bindkey -v
+bindkey "^R" znt-history-widget
+export KEYTIMEOUT=1
+
+# cursor in normal mode differs from insert mode
+cursor_mode() {
+    # See https://ttssh2.osdn.jp/manual/4/en/usage/tips/vim.html for cursor shapes
+    cursor_block='\e[2 q'
+    cursor_beam='\e[6 q'
+
+    function zle-keymap-select {
+        if [[ ${KEYMAP} == vicmd ]] ||
+            [[ $1 = 'block' ]]; then
+            echo -ne $cursor_block
+        elif [[ ${KEYMAP} == main ]] ||
+            [[ ${KEYMAP} == viins ]] ||
+            [[ ${KEYMAP} = '' ]] ||
+            [[ $1 = 'beam' ]]; then
+            echo -ne $cursor_beam
+        fi
+    }
+
+    zle-line-init() {
+        echo -ne $cursor_beam
+    }
+
+    zle -N zle-keymap-select
+    zle -N zle-line-init
+}
+
+cursor_mode
+
+# navigate the completion menu
+zmodload zsh/complist
+bindkey -M menuselect 'h' vi-backward-char
+bindkey -M menuselect 'k' vi-up-line-or-history
+bindkey -M menuselect 'l' vi-forward-char
+bindkey -M menuselect 'j' vi-down-line-or-history
