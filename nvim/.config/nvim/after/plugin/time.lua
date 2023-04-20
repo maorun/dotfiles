56d836776921fdb70201fd3a1b6891b9e83bbbab
Marco Driemel <marco.driemel@axelspringer.com>
2023-04-20 18:09:25 +0200

    config-fix

--- nvim/.config/nvim/after/plugin/time.lua
+++ nvim/.config/nvim/after/plugin/time.lua
@@ -3,7 +3,6 @@ require'telescope'.load_extension('time')
 local wk = require("which-key")
 wk.register({
 t = {
-    name = "Time",
     t = {"<cmd>Telescope time<cr>", "TimePicker"},
 },
 }, { prefix = "<leader>" })
