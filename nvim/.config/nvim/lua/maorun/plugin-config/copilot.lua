38271e94906e03aa316a34d5a09bf0249fcfaa19
Marco Driemel <marco.driemel@axelspringer.com>
2023-04-28 19:38:02 +0200

    refactor(copilot.lua): remove unused keymap The accept keymap is not used in the copilot plugin configuration. Therefore, it has been removed to improve the readability of the configuration file.

--- nvim/.config/nvim/lua/maorun/plugin-config/copilot.lua
+++ nvim/.config/nvim/lua/maorun/plugin-config/copilot.lua
@@ -2,7 +2,6 @@ require("copilot").setup({
     suggestion = {
         -- auto_trigger = true,
         keymap = {
-            accept = "<Tab>",
             next = "<C-n>",
             prev = "<C-p>",
         }
