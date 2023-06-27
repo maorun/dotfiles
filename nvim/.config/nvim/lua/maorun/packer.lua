b41cca7c33f3f9b88636b5611da577ac21c3c2a4
Marco Driemel <marco.driemel@axelspringer.com>
2023-06-27 13:14:05 +0200

    feat(nvim): add other.nvim plugin for navigating between related files The other.nvim plugin is added to the packer configuration. It provides a way to navigate between related files in a project. The configuration sets up mappings for navigating between component, test, story, and MDX files. The plugin is also integrated with which-key for easy access to the mappings.

--- nvim/.config/nvim/lua/maorun/packer.lua
+++ nvim/.config/nvim/lua/maorun/packer.lua
@@ -543,6 +543,59 @@ require('packer').startup(function(use)
         }
     })
 
+    use {
+        'rgroli/other.nvim',
+        config = function()
+            require("other-nvim").setup({
+                rememberBuffers = false,
+                mappings = {
+                    {
+                        pattern = "(.*/)([^%.]+)(%..+)%.tsx$",
+                        target = {
+                            {target = "%1%2.tsx", context = "component" },
+                            {target = "%1%2.component.tsx", context = "component" },
+                            {target = "%1%2.test.tsx", context = "test" },
+                            {target = "%1%2.stories.tsx", context = "story" },
+                            {target = "%1%2.mdx", context = "MDX" },
+                        }
+                    },
+                    {
+                        pattern = "(.*/)([^%.]+)%.mdx$",
+                        target = {
+                            {target = "%1%2.tsx", context = "component" },
+                            {target = "%1%2.component.tsx", context = "component" },
+                            {target = "%1%2.test.tsx", context = "test" },
+                            {target = "%1%2.stories.tsx", context = "story" },
+                            {target = "%1%2.mdx", context = "MDX" },
+                        }
+                    },
+                    {
+                        pattern = "(.*/)([^%.]+)%.tsx$",
+                        target = {
+                            {target = "%1%2.tsx", context = "component" },
+                            {target = "%1%2.component.tsx", context = "component" },
+                            {target = "%1%2.test.tsx", context = "test" },
+                            {target = "%1%2.stories.tsx", context = "story" },
+                            {target = "%1%2.mdx", context = "MDX" },
+                        }
+                    }
+                },
+            })
+            local wk = require("which-key")
+            wk.register({
+                t = {
+                    n = {
+                        name = ":Other",
+                        n = { "<cmd>:Other<cr>", ":Other", noremap = true},
+                        s = { "<cmd>:OtherSplit<cr>", ":OtherSplit", noremap = true},
+                        v = { "<cmd>:OtherVSplit<cr>", ":OtherVSplit", noremap = true},
+                    }
+                },
+            }, { prefix = "<leader>"})
+
+        end
+    }
+
     if packer_bootstrap then
         require('packer').sync()
     end
