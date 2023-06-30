830495c35e3b062994f3c53f843f4db66953e798
Marco Driemel <marco.driemel@axelspringer.com>
2023-06-30 08:47:51 +0200

    feat(packer.lua): add support for null_ls.builtins.diagnostics.eslint feat(packer.lua): make prettier.nvim an optional plugin The null_ls.builtins.diagnostics.eslint source is added to the list of sources for null-ls, enabling linting with ESLint. The prettier.nvim plugin is made optional by setting the opt flag to true, allowing users to choose whether to install it or not.

--- nvim/.config/nvim/lua/maorun/packer.lua
+++ nvim/.config/nvim/lua/maorun/packer.lua
@@ -253,6 +253,7 @@ packer.startup(function(use)
                 sources = {
                     null_ls.builtins.formatting.prettier,
                     null_ls.builtins.formatting.prettier_eslint,
+                    null_ls.builtins.diagnostics.eslint,
                     -- null_ls.builtins.code_actions.gitsigns,
                 },
             })
@@ -263,6 +264,7 @@ packer.startup(function(use)
     use {
         'MunifTanjim/prettier.nvim',
         after = { 'nvim-lspconfig' },
+        opt = true,
         requires = {
             'neovim/nvim-lspconfig',
             'jose-elias-alvarez/null-ls.nvim',
