caee8b7201043c88cdbcf23fcad4499b4155b804
Marco Driemel <marco.driemel@axelspringer.com>
2024-01-03 16:08:41 +0100

    feat(nvim): add code-stats to statusline if loaded
    This commit adds the code-stats plugin to the statusline in NeoVim.
    By displaying the current XP, it allows users to track their progress
    and motivation while coding.

--- nvim/.config/nvim/after/plugin/options.lua
+++ nvim/.config/nvim/after/plugin/options.lua
@@ -44,7 +44,9 @@ vim.opt.statusline=vim.opt.statusline + " %h" -- help-buffer
 vim.opt.statusline=vim.opt.statusline + "%m" -- modified-flag
 vim.opt.statusline=vim.opt.statusline + "%r" -- read-onlyflag
 vim.opt.statusline=vim.opt.statusline + "%="
-vim.opt.statusline=vim.opt.statusline + "%{luaeval(\"require('maorun.code-stats').currentXp()\")} "
+if pcall(require, "maorun.code-stats") then
+    vim.opt.statusline=vim.opt.statusline + "%{luaeval(\"require('maorun.code-stats').currentXp()\")} "
+end
 vim.cmd [[
     "set statusline+=Session:\ %{ObsessionStatus('[active]','[paused]')}
     set statusline+=\ %-14.(%l,%c%V%)\ %P
