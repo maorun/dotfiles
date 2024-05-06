30300fb0f1ab03bdb543679a057dbfe10379022a
Marco Driemel <marco.driemel@axelspringer.com>
2024-05-06 11:14:48 +0200

    extract deepl and use correct global variable

---
+++ .config/nvim/after/plugin/deepl.lua
@@ -0,0 +1,37 @@
+vim.cmd [[
+    function! RequestDeepl(text)
+        let auth = g:deepl_api_key
+        let url = "https://api-free.deepl.com/v2/translate"
+        let header = 'Authorization: DeepL-Auth-Key ' .. auth
+        let contentType = 'Content-Type: application/json'
+        let data = '{ "text": [ "' .. a:text .. '" ], "target_lang": "EN" }'
+
+        let command = 'curl -s -X POST "' .. url .. '" --header "' .. header .. '" --header "' .. contentType .. '" --data ' .. shellescape(data) .. ""
+
+         let result = system(command)
+         let resultJson = json_decode(result)
+         return resultJson.translations[0].text
+    endfunction
+
+    function! DeepL(type)
+        if a:type ==# 'char'
+            execute "normal! `[v`]y"
+            let response = RequestDeepl(@@)
+            execute "normal! `<v`>c" .. (response)
+            echom "translated c"
+        elseif a:type ==# 'v'
+            normal! `<v`>y
+            let response = RequestDeepl(@@)
+            execute "normal! `<v`>c" .. (response)
+            echom "translated v"
+        else
+                " silent execute "normal! `[V`]\"+y"
+            echom a:type ' - ' .. shellescape(@@)
+            return
+        endif
+    endfunction
+
+    nnoremap <silent> <leader>u :set operatorfunc=DeepL<cr>g@
+    vnoremap <leader>u :<c-u>call DeepL(visualmode())<cr>
+
+]]
--- .config/nvim/after/plugin/mappings.lua
+++ .config/nvim/after/plugin/mappings.lua
@@ -178,40 +178,6 @@ vim.cmd [[
         " echo @@ "yanked into clipboard"
     endfunction
 
-    function! RequestDeepl(text)
-        let auth = vim.g.deepl_auth
-        let url = "https://api-free.deepl.com/v2/translate"
-        let header = 'Authorization: DeepL-Auth-Key ' .. auth
-        let contentType = 'Content-Type: application/json'
-        let data = '{ "text": [ "' .. a:text .. '" ], "target_lang": "EN" }'
-
-        let command = 'curl -s -X POST "' .. url .. '" --header "' .. header .. '" --header "' .. contentType .. '" --data ' .. shellescape(data) .. ""
-
-        let result = system(command)
-        let resultJson = json_decode(result)
-        return resultJson.translations[0].text
-    endfunction
-
-    function! DeepL(type)
-        if a:type ==# 'char'
-            execute "normal! `[v`]y"
-            let response = RequestDeepl(@@)
-            execute "normal! `<v`>c" .. (response)
-            echom "translated"
-        elseif a:type ==# 'v'
-            normal! `<v`>y
-            let response = RequestDeepl(@@)
-            execute "normal! `<v`>c" .. (response)
-            echom "translated"
-        else
-                " silent execute "normal! `[V`]\"+y"
-            echom a:type ' - ' .. shellescape(@@)
-            return
-        endif
-    endfunction
-
-    nnoremap <silent> <leader>u :set operatorfunc=DeepL<cr>g@
-    vnoremap <leader>u :<c-u>call DeepL(visualmode())<cr>
 ]]
 
 wk.register({
