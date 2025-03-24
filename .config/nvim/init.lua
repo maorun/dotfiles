a4228619167bdd2695626501a5ba9d9bfc6c9e26
Marco Driemel <marco.driemel@axelspringer.com>
2025-03-24 14:14:45 +0100

    fix(nvim): set local leader key for improved mapping functionality
    Adds the local leader key configuration to enhance custom mapping.
    This adjustment allows for better organization and accessibility of
    keybindings within Neovim, benefiting users who rely on complex
    mappings. Additionally, normalizes quotes in the command for consistency.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,9 @@
-vim.cmd("autocmd BufRead,BufNewFile *.md,*.markdown,*.mdx setlocal filetype=markdown")
+vim.cmd('autocmd BufRead,BufNewFile *.md,*.markdown,*.mdx setlocal filetype=markdown')
 
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 -- disable netrw
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
@@ -38,4 +39,3 @@ vim.api.nvim_create_autocmd('BufWritePost', {
         end
     end,
 })
-
